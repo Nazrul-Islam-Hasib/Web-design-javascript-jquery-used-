218fa14a790b4e4768db058c88336d0ba25230e9
Nazrul-Islam-Hasib <nazrulhasib@gmail.com>
2018-01-18 10:54:28 +0600

    Changes made

--- custom.js
+++ custom.js
@@ -38,271 +38,6 @@ jQuery(document).ready(function(){
      })
 
 
-
-
-//lightbox gallery manually
-
-     function showImage1(){
-       jQuery(".image-bg-gallery").show();
-       jQuery(".imgg1").show();
-       jQuery(".imgg2").hide();
-       jQuery(".imgg3").hide();
-       jQuery(".imgg4").hide();
-       jQuery(".imgg5").hide();
-       jQuery(".imgg6").hide();
-       jQuery(".imgg7").hide();
-       jQuery(".imgg8").hide();
-       jQuery(".imgg9").hide();
-     }
-     function closeImage1(){
-       jQuery(".image-bg-gallery").hide();
-       jQuery(".imgg1").hide();
-     }
-     jQuery(".z1").click(function(){
-      showImage1();   
-     })
-     jQuery(".image-bg-gallery").click(function(){
-      closeImage1();
-     })
-
-
-     function showImage2(){
-       jQuery(".image-bg-gallery").show();
-       jQuery(".imgg2").show();
-       jQuery(".imgg1").hide();
-       jQuery(".imgg3").hide();
-       jQuery(".imgg4").hide();
-       jQuery(".imgg5").hide();
-       jQuery(".imgg6").hide();
-       jQuery(".imgg7").hide();
-       jQuery(".imgg8").hide();
-       jQuery(".imgg9").hide();
-     }
-     function closeImage2(){
-       jQuery(".image-bg-gallery").hide();
-       jQuery(".imgg2").hide();
-     }
-     jQuery(".z2").click(function(){
-      showImage2();   
-     })
-     jQuery(".image-bg-gallery").click(function(){
-      closeImage2();
-     })
-
-     function showImage3(){
-       jQuery(".image-bg-gallery").show();
-       jQuery(".imgg3").show();
-       jQuery(".imgg2").hide();
-       jQuery(".imgg1").hide();
-       jQuery(".imgg4").hide();
-       jQuery(".imgg5").hide();
-       jQuery(".imgg6").hide();
-       jQuery(".imgg7").hide();
-       jQuery(".imgg8").hide();
-       jQuery(".imgg9").hide();
-     }
-     function closeImage3(){
-       jQuery(".image-bg-gallery").hide();
-       jQuery(".imgg3").hide();
-     }
-     jQuery(".z3").click(function(){
-      showImage3();   
-     })
-     jQuery(".image-bg-gallery").click(function(){
-      closeImage3();
-     })
-
-     function showImage4(){
-       jQuery(".image-bg-gallery").show();
-       jQuery(".imgg4").show();
-       jQuery(".imgg2").hide();
-       jQuery(".imgg3").hide();
-       jQuery(".imgg1").hide();
-       jQuery(".imgg5").hide();
-       jQuery(".imgg6").hide();
-       jQuery(".imgg7").hide();
-       jQuery(".imgg8").hide();
-       jQuery(".imgg9").hide();
-     }
-     function closeImage4(){
-       jQuery(".image-bg-gallery").hide();
-       jQuery(".imgg4").hide();
-     }
-     jQuery(".z4").click(function(){
-      showImage4();   
-     })
-     jQuery(".image-bg-gallery").click(function(){
-      closeImage4();
-     })
-
-     function showImage5(){
-       jQuery(".image-bg-gallery").show();
-       jQuery(".imgg5").show();
-       jQuery(".imgg2").hide();
-       jQuery(".imgg3").hide();
-       jQuery(".imgg4").hide();
-       jQuery(".imgg1").hide();
-       jQuery(".imgg6").hide();
-       jQuery(".imgg7").hide();
-       jQuery(".imgg8").hide();
-       jQuery(".imgg9").hide();
-     }
-     function closeImage5(){
-       jQuery(".image-bg-gallery").hide();
-       jQuery(".imgg5").hide();
-     }
-     jQuery(".z5").click(function(){
-      showImage5();   
-     })
-     jQuery(".image-bg-gallery").click(function(){
-      closeImage5();
-     })
-
-     function showImage6(){
-       jQuery(".image-bg-gallery").show();
-       jQuery(".imgg6").show();
-       jQuery(".imgg2").hide();
-       jQuery(".imgg3").hide();
-       jQuery(".imgg4").hide();
-       jQuery(".imgg5").hide();
-       jQuery(".imgg1").hide();
-       jQuery(".imgg7").hide();
-       jQuery(".imgg8").hide();
-       jQuery(".imgg9").hide();
-     }
-     function closeImage6(){
-       jQuery(".image-bg-gallery").hide();
-       jQuery(".imgg6").hide();
-     }
-     jQuery(".z6").click(function(){
-      showImage6();   
-     })
-     jQuery(".image-bg-gallery").click(function(){
-      closeImage6();
-     })
-
-     function showImage7(){
-       jQuery(".image-bg-gallery").show();
-       jQuery(".imgg7").show();
-       jQuery(".imgg2").hide();
-       jQuery(".imgg3").hide();
-       jQuery(".imgg4").hide();
-       jQuery(".imgg5").hide();
-       jQuery(".imgg6").hide();
-       jQuery(".imgg1").hide();
-       jQuery(".imgg8").hide();
-       jQuery(".imgg9").hide();
-     }
-     function closeImage7(){
-       jQuery(".image-bg-gallery").hide();
-       jQuery(".imgg7").hide();
-     }
-     jQuery(".z7").click(function(){
-      showImage7();   
-     })
-     jQuery(".image-bg-gallery").click(function(){
-      closeImage7();
-     })
-
-     function showImage8(){
-       jQuery(".image-bg-gallery").show();
-       jQuery(".imgg8").show();
-       jQuery(".imgg2").hide();
-       jQuery(".imgg3").hide();
-       jQuery(".imgg4").hide();
-       jQuery(".imgg5").hide();
-       jQuery(".imgg6").hide();
-       jQuery(".imgg7").hide();
-       jQuery(".imgg1").hide();
-       jQuery(".imgg9").hide();
-     }
-     function closeImage8(){
-       jQuery(".image-bg-gallery").hide();
-       jQuery(".imgg8").hide();
-     }
-     jQuery(".z8").click(function(){
-      showImage8();   
-     })
-     jQuery(".image-bg-gallery").click(function(){
-      closeImage8();
-     })
-
-     function showImage9(){
-       jQuery(".image-bg-gallery").show();
-       jQuery(".imgg9").show();
-       jQuery(".imgg2").hide();
-       jQuery(".imgg3").hide();
-       jQuery(".imgg4").hide();
-       jQuery(".imgg5").hide();
-       jQuery(".imgg6").hide();
-       jQuery(".imgg7").hide();
-       jQuery(".imgg8").hide();
-       jQuery(".imgg1").hide();
-     }
-     function closeImage9(){
-       jQuery(".image-bg-gallery").hide();
-       jQuery(".imgg9").hide();
-     }
-     jQuery(".z9").click(function(){
-      showImage9();   
-     })
-     jQuery(".image-bg-gallery").click(function(){
-      closeImage9();
-     })
-//lightbox gallery manually ends
-
-//lightbox blog manually
-     function showImage10(){
-       jQuery(".image-bg").show();
-       jQuery(".imgg3").show();
-       jQuery(".imgg7").hide();
-       jQuery(".imgg9").hide();
-     }
-     function closeImage10(){
-       jQuery(".image-bg").hide();
-       jQuery(".imgg3").hide();
-     }
-     jQuery(".zoom1").click(function(){
-      showImage10();   
-     })
-     jQuery(".image-bg").click(function(){
-      closeImage10();
-     })
-
-     function showImage11(){
-       jQuery(".image-bg").show();
-       jQuery(".imgg7").show();
-       jQuery(".imgg9").hide();
-       jQuery(".imgg3").hide();
-     }
-     function closeImage11(){
-       jQuery(".image-bg").hide();
-       jQuery(".imgg7").hide();
-     }
-     jQuery(".zoom2").click(function(){
-      showImage11();   
-     })
-     jQuery(".image-bg").click(function(){
-      closeImage11();
-     })
-
-     function showImage12(){
-       jQuery(".image-bg").show();
-       jQuery(".imgg9").show();
-       jQuery(".imgg3").hide();
-       jQuery(".imgg7").hide();
-     }
-     function closeImage12(){
-       jQuery(".image-bg").hide();
-       jQuery(".imgg9").hide();
-     }
-     jQuery(".zoom3").click(function(){
-      showImage12();   
-     })
-     jQuery(".image-bg").click(function(){
-      closeImage12();
-     })
-//lightbox blog manually ends
  	//counter strats
  	var a = 0;
     $(window).scroll(function() {
@@ -333,43 +68,80 @@ jQuery(document).ready(function(){
       }
     });
 
-    //counter ends
+//counter ends
 
+
+
+//scroll starts
+    jQuery("#about").click(function(){
+        jQuery('html, body').animate({
+          scrollTop: jQuery("#abouts").offset().top
+          }, 2000);
+          return false;
+    })
+    jQuery("#gallery").click(function(){
+        jQuery('html, body').animate({
+          scrollTop: jQuery("#gallerys").offset().top
+          }, 2000);
+          return false;
+    })
+    jQuery("#blog").click(function(){
+        jQuery('html, body').animate({
+          scrollTop: jQuery("#blogs").offset().top
+          }, 2000);
+          return false;
+    })
+    jQuery("#services").click(function(){
+        jQuery('html, body').animate({
+          scrollTop: jQuery("#servicess").offset().top
+          }, 2000);
+          return false;
+    })
+    jQuery("#sponsorship").click(function(){
+        jQuery('html, body').animate({
+          scrollTop: jQuery("#sponsorships").offset().top
+          }, 2000);
+          return false;
+    })
+    jQuery("#contact").click(function(){
+        jQuery('html, body').animate({
+          scrollTop: jQuery("#contacts").offset().top
+          }, 2000);
+          return false;
+    })
+
+
+//scroll ends
+
+//Lightbox starts
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-36251023-1']);
+  _gaq.push(['_setDomainName', 'jqueryscript.net']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+//lightbox ends
+
+
+//up button starts
     jQuery(window).scroll(function(){
-		var distance = jQuery(window).scrollTop();
-	    jQuery(".office").text(distance);
-    });
-    
-    //menu click
-    jQuery("#home").click(function(){
- 		jQuery("html").animate({'scrollTop' : '0'}, 1000);
- 		return false;
- 	});
- 	jQuery("#about").click(function(){
- 		jQuery("html").animate({'scrollTop' : '560'}, 1000);
- 		return false;
- 	});
- 	jQuery("#gallery").click(function(){
- 		jQuery("html").animate({'scrollTop' : '1160'}, 1000);
- 		return false;
- 	});
- 	jQuery("#blog").click(function(){
- 		jQuery("html").animate({'scrollTop' : '2260'}, 1000);
- 		return false;
- 	});
- 	jQuery("#services").click(function(){
- 		jQuery("html").animate({'scrollTop' : '2960'}, 1000);
- 		return false;
- 	});
- 	jQuery("#sponsorship").click(function(){
- 		jQuery("html").animate({'scrollTop' : '3559'}, 1000);
- 		return false;
- 	});
- 	jQuery("#contact").click(function(){
- 		jQuery("html").animate({'scrollTop' : '3960'}, 1000);
- 		return false;
- 	});
+      var distance = jQuery(window).scrollTop(); 
 
+      if (distance > 198) {
+        jQuery(".up-btn").fadeIn();
+      }
+      else{
+        jQuery(".up-btn").fadeOut();
+      }
+    });
+    jQuery(".up-btn").click(function(){
+      jQuery("html").animate({'scrollTop' : 0},2000)
+    })
+//up button ends
 
 
 
@@ -418,14 +190,7 @@ jQuery(document).ready(function(){
  		}
  		else{
  			jQuery("#contact").removeClass("active");
- 		}
-
- 		if (durotto > 555) {
- 			jQuery(".office").fadeIn();
- 		}
- 		else{
- 			jQuery(".office").fadeOut();
- 		} 		
+ 		}		
  	});
     
 });
